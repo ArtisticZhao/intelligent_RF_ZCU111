9fa5bbec57d68ad91db71bfadc8282f3f0e837f4
artisticzhao <hkcreedblacktea@hotmail.com>
2020-04-21 17:51:12 +0800

    BPSK控制器实现并仿真 准备测试

---
+++ verilog_4_bpsk_mod/phase_ctrl.v
@@ -0,0 +1,163 @@
+
+module Phase_Ctrl #(
+    parameter integer data_width = 8,
+    parameter integer frame_length = 150,
+    parameter integer addr_width = 8,
+    parameter integer ref_clk_freq = 100000000,
+    parameter integer baudrate = 9600
+)(
+    input                            clk        , //时钟信号
+    input                            rst_n      , //复位信号
+    // send 启动信号
+    input                            send_signal,
+    // phase ctrl
+    output                           gen_en     ,
+    output reg                       phase_ctrl ,
+    // RAM端口
+    output                           ram_clk    , //RAM时钟
+    input        [data_width-1 : 0]  ram_rd_data, //RAM中读出的数据
+    output  reg                      ram_en     , //RAM使能信号
+    output  reg  [addr_width-1 : 0]  ram_addr   , //RAM地址
+    output  reg  [0:0]               ram_we     , //RAM读写控制信号 1写 0读
+    output  reg  [data_width-1 : 0]  ram_wr_data, //RAM写数据
+    output                           ram_rst      //RAM复位信号,高电平有效
+);
+
+//calculates the clock cycle for baud rate 
+localparam                       CYCLE = ref_clk_freq / baudrate;
+
+//state machine code
+localparam                       S_IDLE       = 1;
+localparam                       S_FIRST_READ = 2; //start bit
+localparam                       S_SEND_BYTE  = 3; //data bits
+localparam                       S_SEND_READ  = 4; //stop bit
+
+// state machine reg
+reg[2:0]                         state;
+reg[2:0]                         bit_cnt;//bit counter
+reg[15:0]                        cycle_cnt; //baud counter
+// 启动信号捕获与转换
+reg          start_rd_d0;
+reg          start_rd_d1;
+wire         pos_start_rd;
+assign pos_start_rd = ~start_rd_d1 & start_rd_d0;
+
+//延时两拍，采start_rd信号的上升沿
+always @(posedge clk or negedge rst_n) begin
+    if(!rst_n) begin
+        start_rd_d0 <= 1'b0;   
+        start_rd_d1 <= 1'b0; 
+    end
+    else begin
+        start_rd_d0 <= send_signal;   
+        start_rd_d1 <= start_rd_d0;     
+    end
+end
+// ---------------------------状态机控制-------------------------------
+always @(posedge clk or negedge rst_n)begin
+    if (!rst_n) begin
+        state <= S_IDLE;
+    end else begin
+        case (state)
+            S_IDLE:
+                if (pos_start_rd) begin
+                    state <= S_FIRST_READ;
+                end else begin
+                    state <= S_IDLE;
+                end
+            S_FIRST_READ:
+                state <= S_SEND_BYTE;
+            S_SEND_BYTE:
+                if (bit_cnt == 7'd0)
+                begin
+                    if(ram_addr == frame_length) begin
+                        state <= S_IDLE;
+                    end else begin
+                        if (cycle_cnt == CYCLE - 1) 
+                            state <= S_SEND_READ;
+                        else
+                            state <= S_SEND_BYTE;;
+                    end
+                end else begin
+                    state <= S_SEND_BYTE;
+                end
+            S_SEND_READ:
+                state <= S_SEND_BYTE;
+        endcase
+    end
+end
+// --------------------------BRAM读取控制------------------------------
+reg [data_width-1 : 0]  data;  // 将要发送的数据
+assign  ram_rst = 1'b0;
+assign  ram_clk= clk;
+always @(posedge clk or negedge rst_n)begin
+    if (!rst_n) begin
+        data <= 8'd0;
+        ram_en <= 1'b0;
+        ram_addr <= 8'd0;
+        ram_we <= 4'd0;
+        ram_en <= 1'b1;
+    end else begin
+        case (state)
+            S_FIRST_READ: begin
+                data <= ram_rd_data;
+                ram_addr <= ram_addr + 8'd1 ;
+            end
+            S_SEND_READ: begin
+                data <= ram_rd_data;
+                ram_addr <= ram_addr + 8'd1 ;
+            end
+            S_IDLE:
+                ram_addr <= 8'd0;
+            default: begin
+                data <= data;
+                ram_addr <= ram_addr;
+            end
+        endcase
+    end
+end
+// --------------------------串行相位控制信号控制------------------------
+// 波特率发生器
+
+always@(posedge clk or negedge rst_n) begin
+	if(rst_n == 1'b0)
+		cycle_cnt <= 16'd0;
+	else if(state != S_SEND_BYTE && state != S_SEND_READ)
+		cycle_cnt <= 16'd0;
+	else if(cycle_cnt != CYCLE)
+		cycle_cnt <= cycle_cnt + 16'd1;	
+    else
+        cycle_cnt <= 16'd0;
+end
+// 字节波特率控制器
+
+always@(posedge clk or negedge rst_n) begin
+	if(rst_n == 1'b0)
+		begin
+			bit_cnt <= 3'd7;
+		end
+	else if(state == S_SEND_BYTE || state == S_SEND_READ)
+		if(cycle_cnt == CYCLE - 1)
+            if (bit_cnt != 3'd0)
+			    bit_cnt <= bit_cnt - 3'd1;
+            else
+                bit_cnt <= 3'd7;
+		else
+			bit_cnt <= bit_cnt;
+	else
+		bit_cnt <= 3'd7;
+end
+// 相位控制信号控制
+always@(posedge clk or negedge rst_n)
+begin
+	if(rst_n == 1'b0)
+		phase_ctrl <= 1'b1;
+	else
+	    if(state == S_SEND_BYTE)
+		    phase_ctrl <= data[bit_cnt];
+        else
+            phase_ctrl <= phase_ctrl;
+end
+// 使能信号
+assign gen_en = (state == S_SEND_BYTE || state == S_SEND_READ);
+endmodule
---
+++ verilog_4_bpsk_mod/tb_phase_ctrl.v
@@ -0,0 +1,56 @@
+`timescale 1ns / 1ns
+
+module tb_phase_ctrl();
+reg clk, rst_n;
+reg send_signal;
+wire gen_en, phase_ctrl;
+wire ram_clk;
+wire [7:0] ram_rd_data;
+wire ram_en;
+wire [7:0] ram_addr;
+wire ram_we;
+wire [7:0] ram_wr_data;
+wire ram_rst;
+initial begin
+    clk=1;
+    rst_n=0;
+    send_signal=0;
+    #10
+    rst_n=1;
+    send_signal=1;
+end
+
+always begin 
+ #5  // 100MHz
+ clk=~clk;
+end
+
+always begin 
+ #62500000  // 100MHz
+ send_signal=~send_signal;
+end
+
+Phase_Ctrl uut(
+    .clk        (clk),
+    .rst_n      (rst_n),
+    .send_signal(send_signal),
+    .gen_en     (gen_en),
+    .phase_ctrl (phase_ctrl),
+    .ram_clk    (ram_clk),
+    .ram_rd_data(ram_rd_data),
+    .ram_en     (ram_en),
+    .ram_addr   (ram_addr),
+    .ram_we     (ram_we),
+    .ram_wr_data(ram_wr_data),
+    .ram_rst    (ram_rst)
+);
+
+blk_mem_gen_0 your_instance_name (
+  .clkb(ram_clk),    // input wire clkb
+  .enb(ram_en),      // input wire enb
+  .web(ram_we),      // input wire [0 : 0] web
+  .addrb(ram_addr),  // input wire [7 : 0] addrb
+  .dinb(ram_wr_data),    // input wire [7 : 0] dinb
+  .doutb(ram_rd_data)  // output wire [7 : 0] doutb
+);
+endmodule
